fdb03ad065aa2b6f32c7d5b7f6e0b11f51c2fc12
Elijah Numa <elijah.numa@gmail.com>
2021-06-23 13:19:28 -0400

    Fixed redundency in copyscripts

--- copyscripts.sh
+++ copyscripts.sh
@@ -1,31 +1,10 @@
 gd=$PWD # git directory
-cd .. && cd /share/belmonte/egnuma/cyms
-cp *.sh $gd/shellscripts
-cp *.csh $gd/cshellscripts
-cp *.txt $gd/textfiles
-cp *.py $gd/pythonscripts
-cp *.cym $gd/cymheadtemplates
-cd ./motortemplates/
-cp *.sh $gd/shellscripts
-cp *.csh $gd/cshellscripts
-cp *.txt $gd/textfiles
-cp *.py $gd/pythonscripts
-cp *.cym $gd/cymheadtemplates
-cd ../../datacollection
-cp *.sh $gd/shellscripts
-cp *.csh $gd/cshellscripts
-cp *.txt $gd/textfiles
-cp *.py $gd/pythonscripts
-cp *.cym $gd/cymheadtemplates
-cd ./data
-cp *.sh $gd/shellscripts
-cp *.csh $gd/cshellscripts
-cp *.txt $gd/textfiles
-cp *.py $gd/pythonscripts
-cp *.cym $gd/cymheadtemplates
-cd ./metadata
-cp *.sh $gd/shellscripts
-cp *.csh $gd/cshellscripts
-cp *.txt $gd/textfiles
-cp *.py $gd/pythonscripts
-cp *.cym $gd/cymheadtemplates
+cd .. && cd /share/belmonte/egnuma
+sd=$PWD # share directory
+for d in $sd/cyms/ $sd/cyms/motortemplates/ $sd/datacollection/ $sd/datacollection/data/
+do cp $d/*.sh $gd/shellscripts
+cp $d/*.csh $gd/cshellscripts
+cp $d/*.txt $gd/textfiles
+cp $d/*.py $gd/pythonscripts
+cp $d/*.cym $gd/cymheadtemplates
+done
