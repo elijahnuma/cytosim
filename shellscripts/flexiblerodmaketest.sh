f582a8901a04dfecd553a449fadbd9cc8de95d97
Elijah Numa <elijah.numa@gmail.com>
2021-08-11 21:17:06 -0400

    Change gitignore

--- shellscripts/flexiblerodmaketest.sh
+++ shellscripts/flexiblerodmaketest.sh
@@ -1,4 +1,4 @@
-for m in 4000 5000 6000 7000 8000 9000 10000
+for m in 100 1000 10000 50000
 do for h in 2 4 6 8 16 32
 do bash ../edit.sh "new [0-9]* fiber minifilament" "new $m fiber minifilament" ${h}headsflexible.cym 
 cp ${h}headsflexible.cym .. && cd .. && bash maketest.sh *.cym 10 && cd ./motortemplates/ 
